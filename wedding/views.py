050d58fb6ad3d1d5714e20df3cf1e8c63b1e59d2
Rob Kimball <rob.kimball@voya.com>
2020-01-21 13:28:07 -0500

    Adds django default structure

---
+++ wedding/views.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+from django.views.generic import TemplateView
+
+
+class SaveDateView(TemplateView):
+    template_name = 'save-the-date.tpl'
+
+
+class RSVPView(TemplateView):
+    template_name = 'rsvp.tpl'
