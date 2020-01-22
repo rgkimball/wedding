01626a5283cfda91915e68cf267ed9fe64fca45b
Rob Kimball <kimball.r@me.com>
2020-01-21 23:28:29 -0500

    Various theme updates

--- wedding/views.py
+++ wedding/views.py
@@ -6,8 +6,8 @@ from django.views.generic import TemplateView
 
 
 class SaveDateView(TemplateView):
-    template_name = 'save-the-date.tpl'
+    template_name = 'save-the-date.html'
 
 
 class RSVPView(TemplateView):
-    template_name = 'rsvp.tpl'
+    template_name = 'rsvp.html'
